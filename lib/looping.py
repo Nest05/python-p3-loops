5890e4ba81efbe7d35eff2f7e57b71e5c4682720
Nestor Masinde <nestormasinde@gmail.com>
2024-02-27 13:59:37 +0300

    loops covered

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    n = 10
+    while n >= 1:
+        n -= 1
+        print(n + 1)
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    int_list = [n * n for n in int_list]
+    return int_list
 def fizzbuzz():
-    # code goes here!
-    pass
+    n = 1
+    while n <= 100:
+        if (n % 3 == 0) and (n % 5 == 0):
+            print("FizzBuzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        elif n % 5 == 0:
+            print("Buzz")
+        else:
+            print(n)
+        n += 1
